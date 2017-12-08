8f979ba7cddcb5373fe7fc7daa19a60378cc0bd7
Jesse Bernier <jesse_bernier@hotmail.com>
2017-12-08 14:32:57 -0500

    working files

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+{
+  "development": {
+    "username": "root",
+    "password": "T3ddyT3ssa",
+    "database": "burgers_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "T3ddyT3ssa",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "T3ddyT3ssa",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
